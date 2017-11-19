6018ce513b17c7e7d9edb84e6d2fef811d864c71
codebicycle <codebicycle@gmail.com>
2017-11-19 18:42:40 +0200

    Create SiteAdmin

--- sites/admin.py
+++ sites/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django import forms
 
-# Register your models here.
+from .models import Site
+
+
+class SiteAdminForm(forms.ModelForm):
+    class Meta:
+        model = Site
+        widgets = {
+            'password': forms.PasswordInput(render_value = True)
+        }
+        fields = '__all__'
+
+
+@admin.register(Site)
+class SiteAdmin(admin.ModelAdmin):
+    form = SiteAdminForm
+    fields = (
+        'name',
+        'login_url',
+        ('username', 'username_label'),
+        ('password', 'password_label'),
+        'success',
+    )
