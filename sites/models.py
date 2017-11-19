1bb034a748587a9444e004edd65e38ee9d8ea746
codebicycle <codebicycle@gmail.com>
2017-11-19 18:41:53 +0200

    Create Site model

--- sites/models.py
+++ sites/models.py
@@ -1,3 +1,16 @@
+from django import forms
 from django.db import models
+from encrypted_model_fields.fields import EncryptedCharField
 
-# Create your models here.
+
+class Site(models.Model):
+    name = models.CharField(max_length=200)
+    login_url = models.URLField()
+    username = models.CharField(max_length=200)
+    username_label = models.CharField(max_length=200, default='username')
+    password = EncryptedCharField(max_length=200)
+    password_label = models.CharField(max_length=200, default='password')
+    success = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.name)
